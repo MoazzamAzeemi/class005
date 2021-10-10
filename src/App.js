7268025aabddb44edbf333a7755b1756a37495f6
MoazzamAzeemi <38378900+MoazzamAzeemi@users.noreply.github.com>
2021-10-10 00:19:51 -0700

    App is structured for class 5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Parent from "./Parent";
 
 function App() {
   return (
     <div className="App">
+      <p>This is the main App</p>
+      <Parent />
     </div>
   );
 }
---
+++ src/Child1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Child1() {
+    return (
+      <div className="">
+        <p>This is Child1</p>        
+      </div>
+    );
+  }
+  
+export default Child1;
